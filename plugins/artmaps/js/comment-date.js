e962a20c1ce9a63d07b6d6b42729dbfe5231f53a
Dominic Price <dominic.price@nottingham.ac.uk>
2013-08-13 18:13:32 +0100

    Attempt to localize times in browser

--- plugins/artmaps/js/comment-date.js
+++ plugins/artmaps/js/comment-date.js
@@ -1,13 +1,17 @@
 jQuery(function($) {
-   $("#comments").find("time").each(function(i, e) {
-       $(e).timeago();
-   });
-   $("#comments").find(".comment-author").contents().filter(
-           function() { 
-               return this.nodeType == Node.TEXT_NODE; 
-           }).each(
-                   function(i, e) {
-       if(e.data.indexOf("on") > -1)
-           e.data = " ";
-   });
+    $("time").each(function(i, e) {
+        var d = new Date($(e).attr("datetime"));
+        $(e).attr("title", d.toLocaleString());
+    });
+    $("#comments").find("time").each(function(i, e) {
+        $(e).timeago();
+    });
+    $("#comments").find(".comment-author").contents().filter(
+            function() {
+                return this.nodeType == Node.TEXT_NODE;
+            }).each(
+                    function(i, e) {
+                        if(e.data.indexOf("on") > -1)
+                            e.data = " ";
+                    });
 });
