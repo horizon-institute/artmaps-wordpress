94483fdc6ece32a0fc7fe5a02e5fbca43aa32178
Dominic Price <dominic.price@nottingham.ac.uk>
2013-08-22 15:50:58 +0100

    Fixed a bug where jQuery was not being registered properly for admin pages

--- plugins/artmaps/classes/ArtMapsBlogAdmin.php
+++ plugins/artmaps/classes/ArtMapsBlogAdmin.php
@@ -11,8 +11,16 @@ class ArtMapsBlogAdmin {
                 'artmaps-blog-admin-page',
                 array($this, 'display'));
         add_action('admin_print_scripts-' . $sfx, function() {
+            $p = plugins_url(basename(dirname(dirname(__FILE__))));
+            wp_register_script('jquery-ui-complete', "https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js");
+            wp_register_script('jquery-ui-timepicker-addon', $p . '/js/lib/jquery-ui-timepicker-addon.js');
             wp_enqueue_script('jquery-ui-complete');
             wp_enqueue_script('jquery-ui-timepicker-addon');
+            require_once('ArtMapsNetwork.php');
+            $nw = new ArtMapsNetwork();
+            $blog = $nw->getCurrentBlog();
+            wp_register_style('jquery-theme', $blog->getJQueryThemeUri());
+            wp_enqueue_style('jquery-theme');
         });
     }
 
