6c6b70b166796a6195e41508fbf4cfe121c7a30a
Dominic Price <dominic.price@nottingham.ac.uk>
2014-02-19 14:36:46 +0000

    Corrected dynimage domain name

--- themes/artmaps/single-artwork.php
+++ themes/artmaps/single-artwork.php
@@ -57,7 +57,7 @@ jQuery(function($) {
 
     <div class="artmaps-object-image">
       <?php if(get_post_meta(get_the_ID(),"imageurl",true)) { ?>
-        <img src="http://dev.artmaps.org.uk/artmaps/tate/dynimage/y/250/<?php echo get_post_meta(get_the_ID(),"imageurl",true); ?>" data-full-image="<?php echo get_post_meta(get_the_ID(),"imageurl",true); ?>" alt="<?php the_title(); ?>" class="artwork-img" />
+        <img src="//artmaps.tate.org.uk/artmaps/tate/dynimage/y/250/<?php echo get_post_meta(get_the_ID(),"imageurl",true); ?>" data-full-image="<?php echo get_post_meta(get_the_ID(),"imageurl",true); ?>" alt="<?php the_title(); ?>" class="artwork-img" />
       <?php } else { ?>
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/unavailable.png" alt="<?php the_title(); ?>" class="artwork-img" />
       <?php } ?>
